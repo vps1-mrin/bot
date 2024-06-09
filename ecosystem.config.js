246fcb4d4e867556b140f6da7ebc64f16c10096c
vps1-mrin <mrinvps1@gmail.com>
2024-06-10 01:41:29 +0530

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,6 @@
 module.exports = {
   apps: [
     {
-      name: "bgmi",
-      script: "./bgmi",
-      args: "<target_ip> <target_port> <attack_duration>",
-      autorestart: false
-    },
-    {
       name: "Telegram Bot",
       script: "python3",
       args: "mrin.py",
